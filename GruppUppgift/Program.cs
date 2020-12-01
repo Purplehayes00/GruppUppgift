197cbfb4222feadd5064453cb473756886ba6c67
Johan Rohdin <Johanrohdin@hotmail.com>
2020-12-01 09:10:27 +0100

    Add project files.

---
+++ GruppUppgift/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GruppUppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej och välkommen till inlämningsuppgift2! \n" +
+                "Skriv vilken grupp du tillhör: ");
+
+
+
+
+
+
+
+
+        }
+    }
+}
